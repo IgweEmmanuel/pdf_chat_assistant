3f28b8d7eec2c4c0e363976ced87b773906d2e91
Emmanuel Igwe <136444225+IgweEmmanuel@users.noreply.github.com>
2024-09-23 21:06:59 +0100

    Update create_asset.py

--- create_asset.py
+++ create_asset.py
@@ -1,1 +1,32 @@
+import bagel
+import os
 
+client = bagel.Client()
+
+
+# Set environment variable
+os.environ['BAGEL_API_KEY']
+
+
+def create_asset(client):
+    title = "creamcheese_dataset_chatbot_test" #@param
+    description = "Cream Cheese dataset_test" #@param
+    user_id = '136444225'
+    payload = {
+                "title": title,
+                "dataset_type": "RAW",
+                "tags": [
+                    "AI", "TEST"
+                ],
+                "category": "AI",
+                "details": description,
+                "user_id": user_id
+            }
+
+    dataset = client.create_asset(
+        payload=payload
+    )
+    return dataset
+
+asset_id = create_asset(client)
+print(asset_id)
