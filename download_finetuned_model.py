b40b348bd3d5ccb8ce5c52d7ecc9cee24764740e
Emmanuel Igwe <136444225+IgweEmmanuel@users.noreply.github.com>
2024-09-23 21:09:58 +0100

    Update download_finetuned_model.py

--- download_finetuned_model.py
+++ download_finetuned_model.py
@@ -1,1 +1,11 @@
+import bagel
+import os
 
+client = bagel.Client()
+
+# Set environment variable
+os.environ['BAGEL_API_KEY']
+
+finetuned_model_id = '9fbc2581-6a92-46fa-acb5-003b9efebac6'
+response = client.download_model(finetuned_model_id)
+print(response)
