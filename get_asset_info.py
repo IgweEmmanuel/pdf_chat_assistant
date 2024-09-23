53013fc78d38071ebdc5902e0b9dbf5bedcf72cb
Emmanuel Igwe <136444225+IgweEmmanuel@users.noreply.github.com>
2024-09-23 21:11:22 +0100

    Update get_asset_info.py

--- get_asset_info.py
+++ get_asset_info.py
@@ -1,1 +1,12 @@
+import bagel
+import os
 
+client = bagel.Client()
+
+# Set environment variable
+os.environ['BAGEL_API_KEY']
+
+asset_id = 'f501e8fd-73c8-4b97-9b51-d934814ff987'
+
+asset_info = client.get_asset_info(asset_id)
+print(asset_info)
