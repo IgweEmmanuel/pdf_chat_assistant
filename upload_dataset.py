ea92b830cabb2b7b1c5cec918f49393b3f7eb989
Emmanuel Igwe <136444225+IgweEmmanuel@users.noreply.github.com>
2024-09-23 21:15:04 +0100

    Update upload_dataset.py

--- upload_dataset.py
+++ upload_dataset.py
@@ -1,1 +1,17 @@
+import bagel
+import os
 
+client = bagel.Client()
+
+# Set environment variable
+os.environ['BAGEL_API_KEY']
+
+asset_id = 'f501e8fd-73c8-4b97-9b51-d934814ff987'
+
+file_path = 'generated_from_txt.parquet'
+
+response = client.file_upload(
+                            file_path=file_path,
+                            asset_id=asset_id
+                          )
+print(response)
