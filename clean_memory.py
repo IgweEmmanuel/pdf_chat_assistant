b9dd2f96798402bcac3618c30206a8dfc0c440c9
Emmanuel Igwe <136444225+IgweEmmanuel@users.noreply.github.com>
2024-09-23 21:08:07 +0100

    Update clean_memory.py

--- clean_memory.py
+++ clean_memory.py
@@ -1,1 +1,11 @@
+import gc
+import torch
+import load_transformers
+del load_transformers.model
+del load_transformers.tokenizer
 
+
+torch.cuda.empty_cache()
+gc.collect()
+torch.cuda.reset_peak_memory_stats()
+torch.cuda.empty_cache()
