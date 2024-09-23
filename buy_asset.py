3a7e063c9a116abcbfde250df91caaee4ac1dc39
Emmanuel Igwe <136444225+IgweEmmanuel@users.noreply.github.com>
2024-09-23 21:05:51 +0100

    Update buy_asset.py

--- buy_asset.py
+++ buy_asset.py
@@ -1,1 +1,16 @@
+import bagel
+import os
 
+client = bagel.Client()
+
+# Set environment variable
+os.environ['BAGEL_API_KEY']
+
+user_id = '136444225'
+
+llama3_model_id = "3323b6c4-06ef-4949-b239-1a2b220e211d" #This is LLama3-8b model
+response = client.buy_asset(
+                    asset_id=llama3_model_id,
+                    user_id=user_id
+                )
+print(response)
